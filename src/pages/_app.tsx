0a23f3c43d70b3d3a73320d0da8a6d461b61da21
robivr <vrbarobi@gmail.com>
2023-06-28 20:38:52 +0200

    initial commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,15 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css"
+import type { AppProps } from "next/app"
+
+const Footer = () => {
+  return <footer>Some footer content</footer>
+}
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <Footer />
+    </>
+  )
 }
