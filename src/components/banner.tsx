912da44399ce90c09bd9ff3e076f068904386b69
robivr <vrbarobi@gmail.com>
2023-06-28 21:10:53 +0200

    early steps

---
+++ src/components/banner.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+import styles from './banner.module.css'
+
+type BannerProps = {
+  buttonText: string
+  onClickHandler: (e: React.MouseEvent<HTMLButtonElement>) => void
+}
+
+const Banner = (props: BannerProps) => {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>
+        <span className={styles.title1}>Coffee</span>
+        <span className={styles.title2}>Connoisseur</span>
+      </h1>
+      <p className={styles.subtitle}>Discover your local coffee shops!</p>
+      <button className={styles.button} onClick={props.onClickHandler}>
+        {props.buttonText}
+      </button>
+    </div>
+  )
+}
+
+export default Banner
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,9 +3,18 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 
+import Banner from '@/components/banner'
+import React from 'react'
+
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const handleOnBannerButtonClick = (
+    e: React.MouseEvent<HTMLButtonElement>,
+  ) => {
+    console.log('clicked the banner button')
+  }
+
   return (
     <>
       <Head>
@@ -15,7 +24,10 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={`${styles.main} ${inter.className}`}>
-        <h1 className={styles.title}>Coffee Connoisseur</h1>
+        <Banner
+          buttonText="View nearby stores"
+          onClickHandler={handleOnBannerButtonClick}
+        />
       </main>
     </>
   )
