6665cc8bd03da5633403e87e6e41b715716e20fb
robivr <vrbarobi@gmail.com>
2023-07-11 13:12:03 +0200

    routes exercise

--- src/pages/coffee-store/[id].tsx
+++ src/pages/coffee-store/[id].tsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import { useRouter } from 'next/router'
+import Link from 'next/link'
 
 const CoffeeStore = () => {
   const router = useRouter()
   const { id } = router.query
   console.log({ router })
 
-  return <div>coffee-store page {id}</div>
+  return (
+    <div>
+      coffee-store page {id}
+      <Link href="/">Back to home</Link>
+      <Link href="/coffee-store/2">Go to dynamic page</Link>
+      <Link href="/courses/nextjs">Go to NextJS page</Link>
+    </div>
+  )
 }
 
 export default CoffeeStore
---
+++ src/pages/courses/nextjs.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Nextjs = () => {
+  return <div>Welcome to Next.js</div>
+}
+
+export default Nextjs
