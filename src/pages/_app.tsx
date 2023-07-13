cf3f7d47b74d17d3cabc19d1f128e570f9c1e7ec
robivr <vrbarobi@gmail.com>
2023-07-13 14:05:36 +0200

    added new font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,6 @@
-import "@/styles/globals.css"
-import type { AppProps } from "next/app"
-
-const Footer = () => {
-  return <footer>Some footer content</footer>
-}
+import '@/styles/globals.css'
+import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <Component {...pageProps} />
-      <Footer />
-    </>
-  )
+  return <Component {...pageProps} />
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,20 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link
+          rel="preload"
+          href="/fonts/JosefinSans-VariableFont_wght.ttf"
+          as="font"
+          crossOrigin="anonymous"
+        />
+        <link
+          rel="preload"
+          href="/fonts/JosefinSans-Italic-VariableFont_wght.ttf"
+          as="font"
+          crossOrigin="anonymous"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
