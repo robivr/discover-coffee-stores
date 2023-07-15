ae84461c471ab59b253d31c7693b4811b490e57c
robivr <vrbarobi@gmail.com>
2023-07-15 10:35:21 +0200

    added getStaticPath to CoffeeStore

--- src/pages/coffee-store/[id].tsx
+++ src/pages/coffee-store/[id].tsx
@@ -1,8 +1,42 @@
 import React from 'react'
 import { useRouter } from 'next/router'
 import Link from 'next/link'
+import { GetStaticPropsContext } from 'next'
 
-const CoffeeStore = () => {
+import coffeeStoresData from '../../data/coffee-stores.json'
+
+export function getStaticProps({ params }: { params: { id: string } }) {
+  console.log('params', params)
+  return {
+    props: {
+      coffeeStore: coffeeStoresData.find(
+        (coffeeStore) => coffeeStore.id.toString() === params.id,
+      ),
+    },
+  }
+}
+
+export function getStaticPaths() {
+  return {
+    paths: coffeeStoresData.map(({ id }) => ({
+      params: { id: id.toString() },
+    })),
+    fallback: false,
+  }
+}
+
+type CoffeeStoreProps = {
+  coffeeStore: {
+    name: string
+    imgUrl: string
+    href: string
+    id: number
+    address: string
+  }
+}
+
+const CoffeeStore = (props: CoffeeStoreProps) => {
+  console.log('CoffeeStore props', props)
   const router = useRouter()
   const { id } = router.query
   console.log({ router })
@@ -13,6 +47,8 @@ const CoffeeStore = () => {
       <Link href="/">Back to home</Link>
       <Link href="/coffee-store/2">Go to dynamic page</Link>
       <Link href="/courses/nextjs">Go to NextJS page</Link>
+      <p>{props.coffeeStore.address}</p>
+      <p>{props.coffeeStore.name}</p>
     </div>
   )
 }
