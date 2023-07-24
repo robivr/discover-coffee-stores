ff666e054830199e3adaacc6d9916d87ff94ce08
robivr <vrbarobi@gmail.com>
2023-07-24 11:23:35 +0200

    created record on airtable

--- src/pages/api/createCoffeeStore.ts
+++ src/pages/api/createCoffeeStore.ts
@@ -1,6 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-import Airtable, { FieldSet, Records } from 'airtable'
+import Airtable from 'airtable'
 const base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(
   process.env.AIRTABLE_BASE_KEY as string,
 )
@@ -8,29 +8,66 @@ const base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(
 const table = base('coffee-stores')
 console.log(table)
 
-type ResponseData =
-  | {
-      message?: string
-    }
-  | Records<FieldSet>
+type CoffeeStoreRecord = {
+  imgUrl: string
+  name: string
+  id: string
+  address: string
+  locality: string
+  rating: number
+}
+
+type Message = {
+  error?: string
+  message?: string
+  // [x: string]: any
+}
 
 const createCoffeeStore = async (
   req: NextApiRequest,
-  res: NextApiResponse<ResponseData>,
+  res: NextApiResponse<CoffeeStoreRecord[] | Message>,
 ) => {
-  // console.log(req)
+  console.log('request', req.body)
 
   if (req.method === 'POST') {
-    const findCoffeeStoreRecords = await table
-      .select({
-        filterByFormula: `id="1"`,
+    try {
+      const findCoffeeStoreRecords = await table
+        .select({
+          filterByFormula: `id="2"`,
+        })
+        .firstPage()
+
+      const records = findCoffeeStoreRecords.map((record) => {
+        return { ...record.fields } as CoffeeStoreRecord
       })
-      .firstPage()
 
-    console.log({ findCoffeeStoreRecords })
+      console.log({ findCoffeeStoreRecords })
 
-    if (findCoffeeStoreRecords.length > 0) {
-      res.json(findCoffeeStoreRecords)
+      if (findCoffeeStoreRecords.length > 0) {
+        res.json(records)
+      } else {
+        const createRecords = await table.create([
+          {
+            fields: {
+              id: '2',
+              name: 'My favorite coffee store',
+              address: 'road 5',
+              locality: 'New York',
+              rating: 200,
+              imgUrl: 'some-image.png',
+            },
+          },
+        ])
+
+        const records = createRecords.map((record) => {
+          return { ...record.fields } as CoffeeStoreRecord
+        })
+
+        res.json(records)
+      }
+    } catch (err) {
+      console.error('Error finding store', err)
+      res.status(500).json({ error: 'Something went wrong' })
     }
   }
 }
